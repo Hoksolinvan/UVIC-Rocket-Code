151b17f7e62334a8191299a62d82d0db3d747362
Hoksolinvan Chhun <158796823+Hoksolinvan@users.noreply.github.com>
2026-07-09 12:36:29 -0700

    Update w25q32jv.c

--- Flash_driver/w25q32jv.c
+++ Flash_driver/w25q32jv.c
@@ -10,3 +10,24 @@ void send_write_enable(void)
     spi_transfer(tx, NULL, 1);  
     spi_cs_high();   
 }
+
+
+void volatile_write_enable(void){
+    uint8_t tx[1] = {VOLATILE_SR_WRITE_ENABLE};
+
+    spi_cs_low();
+    spi_transfer(tx,NULL,1);
+    spi_cs_high();
+
+}
+
+
+void write_disable(void){
+    uint8_t tx[1] = {WRITE_DISABLE};
+
+    spi_cs_low();
+    spi_transfer(tx,NULL,1);
+    spi_cs_high();
+
+
+}
